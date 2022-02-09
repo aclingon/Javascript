a95aa72c99859c53f85c5cfa7ce007586f4b85a0
aclingon <anneclingon@gmail.com>
2022-02-09 14:25:33 -0800

    javascript updates

---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,16 @@
+
+var W = "Wow a string!";
+window.alert(W);
+document.write(" I told the dogs \" Sit down! \" But they didn't listen! ");
+
+var Q = "Chandler " + "and " + "Winston "
+document.write("My dogs names are " + Q ); 
+
+var Family = "The Clingons", Man = "Chris", Woman = "Anne ", Dogs = "Winston and Chandler "
+document.write( Woman );
+
+document.write( 8*7)
+
+var str = 'This will be blue';
+document.write(str.fontcolor(yellow));
+

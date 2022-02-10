330fd17254a1d06cecadf505b574343a1ced904b
aclingon <anneclingon@gmail.com>
2022-02-09 20:23:53 -0800

    project 2

---
+++ Basic Javascript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+function My_Funky_Function() { //creating function//
+    var str = "This is what is under my button! ", t = "Isnt it cool?"; //assigning variables//
+    document.getElementById("Button_Text").innerHTML = str + t; //calling to the html//
+}
+
+function My_Function() { //creating function//
+    var phrase = "I am leanring"; //assigning variable//
+    phrase += " how to code in javascript!"; //concenating variable and string of text//
+    document.getElementById("trial").innerHTML = phrase; //calling to the html file//
+}

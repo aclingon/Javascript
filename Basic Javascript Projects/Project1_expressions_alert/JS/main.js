f4df0c5fe7a91862e67359c04a7829b6a611724c
aclingon <anneclingon@gmail.com>
2022-02-09 19:40:35 -0800

    edits

--- Basic Javascript Projects/Project1_expressions_alert/JS/main.js
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -1,16 +1,16 @@
 
-var W = "Wow a string!";
-window.alert(W);
-document.write(" I told the dogs \" Sit down! \" But they didn't listen! ");
+var W = "Wow a string!"; //defining a variable//
+window.alert(W); //alert for the variable above//
+document.write(" I told the dogs \" Sit down! \" But they didn't listen! "); //combing a few strings of text//
 
-var Q = "Chandler " + "and " + "Winston "
-document.write("My dogs names are " + Q ); 
+var Q = "Chandler " + "and " + "Winston " //defining more variables//
+document.write("My dogs names are " + Q );  
 
-var Family = "The Clingons", Man = "Chris", Woman = "Anne ", Dogs = "Winston and Chandler "
-document.write( Woman );
+var Family = "The Clingons", Man = "Chris", Woman = "Anne ", Dogs = "Winston and Chandler " //assinging multiple variables at once//
+document.write("My name is " + Woman + " and my dogs names are " + Dogs); //concenating multiple varibales with text//
 
-document.write( 8*7)
+document.write( 8*7) //an expression//
 
-var str = 'This will be blue';
-document.write(str.fontcolor(yellow));
+var str = 'This will be blue'; //making a new variable//
+document.write(str.fontcolor("yellow")); //changing the above variable to be in a new color//
 
